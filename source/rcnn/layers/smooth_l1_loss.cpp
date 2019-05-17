12f29ee49511469a82cc04f5c82f581cff5247e7
Linus <lsrock1@naver.com>
2019-05-17 21:07:36 +0900

    Update smooth_l1_loss.cpp

--- source/rcnn/layers/smooth_l1_loss.cpp
+++ source/rcnn/layers/smooth_l1_loss.cpp
@@ -5,9 +5,8 @@ namespace rcnn{
 namespace layers{
 torch::Tensor smooth_l1_loss(torch::Tensor input, torch::Tensor target, float beta, bool size_average){
     torch::Tensor n = torch::abs(input - target);
-    auto cond =  n < beta;
-    torch::Tensor loss = cond * ((0.5 * n).pow(2) / beta) + (cond == 0) * (n - 0.5 * beta);
-    // torch::Tensor loss = torch::where(cond, (0.5 * n).pow(2) / beta, n - 0.5 * beta);
+    auto cond = n < beta;
+    torch::Tensor loss = cond * ((0.5 * n).pow(2) / beta) + (1 - cond) * (n - 0.5 * beta);
     if(size_average){
         return loss.mean();
     }
